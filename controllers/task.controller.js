6c4415105aa1e2486360c16d66de093e245d64e7
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2023-12-08 14:18:45 +0700

    feat: add route and logic for edit task

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -44,4 +44,30 @@ const deleteTask = async (req, res) => {
   }
 };
 
-export default { addTask, getTask, deleteTask };
+//Edit Task
+const editTask = async (req, res) => {
+  try {
+    const { title, desc, date, status } = req.body;
+
+    //Convert date from string to date format
+    const dateParts = date.split("/");
+    const newDate = new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+
+    const task = await Task.findByIdAndUpdate(
+      req.params.id,
+      {
+        title,
+        desc,
+        status,
+        date: newDate,
+      },
+      { new: true }
+    );
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+export default { addTask, getTask, deleteTask, editTask };
--- routes/task.route.js
+++ routes/task.route.js
@@ -6,5 +6,6 @@ const taskRouter = express.Router();
 taskRouter.post('/task', taskController.addTask);
 taskRouter.get('/task', taskController.getTask);
 taskRouter.delete('/task/:id', taskController.deleteTask);
+taskRouter.put('/task/:id', taskController.editTask);
 
 export default taskRouter;
