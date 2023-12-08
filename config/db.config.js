2e9bd01234acf886ef91d57b7520a7c45d134ec7
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2023-12-08 17:24:52 +0700

    feat: change config

--- config/db.config.js
+++ config/db.config.js
@@ -6,4 +6,4 @@ const password = process.env.DB_PASS;
 const cluster = process.env.DB_CLUSTER;
 const dbname = process.env.DB_NAME;
 
-export const url = `mongodb+srv://${username}:${password}@${cluster}.emufjmv.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+export const url = `mongodb+srv://${"dummyacc"}:${"MTJRrOxoKiPMxvZ1"}@${"adt-test"}.emufjmv.mongodb.net/${"adt-test"}?retryWrites=true&w=majority`;
